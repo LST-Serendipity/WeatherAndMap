8559057c071c6f482d95ddad362750cdaa216b85
LST-Serendipity <LST3442410178@Outlook.com>
2025-03-27 22:44:36 +0800

    Delete src directory

--- src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter,createWebHistory,createWebHashHistory } from "vue-router";
-import { constantRoute } from "./routes";
-
-const router=createRouter({
-    history:createWebHashHistory(),
-    routes:constantRoute
-})
-
-export default router
--- src/unit/mapControl.ts
+++
@@ -1,9 +0,0 @@
-import FullScreen from 'ol/control/FullScreen.js';
-
-export default function setControl(map,id){
-
-    const fullScreen=new FullScreen({
-        target:id
-    })
-    map.addControl(fullScreen)
-}
